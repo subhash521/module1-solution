bac93187644f5753a9af7e10cb393de981ef0aaa
subhash521 <subhashpradhan521@gmail.com>
2017-04-01 00:10:00 -0400

    'assignment'

---
+++ app.js
@@ -0,0 +1,33 @@
+(function (){
+'use strict';
+angular.module('LunchCheck',[])
+.controller('LunchCheckController', function($scope){
+
+  $scope.message="";
+
+  $scope.status=function (){
+    var inputString=$scope.name;
+    console.log(inputString);
+    if (inputString==undefined){
+      $scope.message="Please enter first data";
+    }else{
+   var arrayOfString= inputString.split(",");
+   for(var i=0; i<arrayOfString.length;i++){
+     if (arrayOfString[i]==""){
+       arrayOfString.splice(i);
+
+     }
+   }
+  // console.log(arrayOfString.length);
+   if (arrayOfString.length<=3){
+  $scope.message="Enjoy!";
+   }
+   else{
+     $scope.message="Too much!";
+   }
+}
+  };
+
+});
+
+})();
